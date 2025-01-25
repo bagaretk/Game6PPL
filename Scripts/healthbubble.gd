824e887b717fa8af544f7857602b2ebe9aff33dc
Pele George-Valentin <george.pele@student.upt.ro>
2025-01-25 17:49:09 +0200

    Attack + Health

--- Scripts/healthbubble.gd
+++ Scripts/healthbubble.gd
@@ -1,23 +1,17 @@
-# HealthBubble.gd
 extends CharacterBody2D
 
+class_name HealthBubble;
+
 @export var speed: float = 100.0
-@export var wave_amplitude: float = 50.0   # How far left/right it moves
-@export var wave_frequency: float = 2.0    # How quickly it oscillates horizontally
+@export var wave_amplitude: float = 50.0
+@export var wave_frequency: float = 2.0
 
 var time := 0.0
 
 func _physics_process(delta: float) -> void:
 	time += delta
-	# Move up:
-	velocity.y = -speed
 
-	# Add a wave motion on the x-axis:
-	# You can use sine for smooth left-right oscillation.
+	velocity.y = -speed
 	velocity.x = sin(time * wave_frequency) * wave_amplitude
 
 	move_and_slide()
-
-	# If the enemy goes off the top of the screen by some margin, remove it.
-	if position.y < -100.0:
-		queue_free()
