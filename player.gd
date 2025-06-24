cb63d4643019cb0d6bace0a33f8ea8217561ce91
Andrei Negriu <81017870+NegriuAndrei@users.noreply.github.com>
2025-06-24 22:04:48 +0300

    GamePrototype

---
+++ player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Obține direcția de intrare pe axele X și Y
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+
+	# Setează viteza pe baza direcției
+	velocity.x = direction_x * SPEED
+	velocity.y = direction_y * SPEED
+
+	# Aplica mișcarea
+	move_and_slide()
